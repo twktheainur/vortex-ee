731f30e8d8027077127d6d0ebc681106b9abdce7
guillaume.ardaud <guillaume.ardaud@37e2baaa-b253-11dd-9381-bf584fb1fa83>
2008-12-11 22:53:09 +0000

    Nettoyage de code (pas encore opé)

---
+++ src/vortex-client/application.h
@@ -0,0 +1,39 @@
+#ifndef _APPLICATION_H
+#define _APPLICATION_H
+
+#include <Ogre.h>
+#include <OIS/OIS.h>
+
+#include "exitlistener.h"
+
+using namespace Ogre;
+
+class Application
+{
+public:
+    void go();
+    ~Application();
+
+private:
+    Root *mRoot;
+    OIS::Keyboard *mKeyboard;
+    OIS::InputManager *mInputManager;
+    SceneManager *mSceneMgr;
+    Camera *mCamera;
+    RenderWindow *win;
+    //CEGUI::OgreCEGUIRenderer *mRenderer;
+    //CEGUI::System *mSystem;
+    ExitListener *mListener;
+
+    void createRoot();
+    void defineResources();
+    void setupRenderSystem();
+    void createRenderWindow();
+    void initializeResourceGroups();    void setupScene();
+    void setupInputSystem();
+//    void setupCEGUI();
+    void createFrameListener();
+    void startRenderLoop();
+};
+
+#endif
