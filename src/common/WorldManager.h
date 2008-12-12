d5b284905b3d7f012adc365c25ac0495d4e91881
twk.theainur <twk.theainur@37e2baaa-b253-11dd-9381-bf584fb1fa83>
2008-12-12 17:43:47 +0000

    Moved WorldManager from vortex-server to common

---
+++ src/common/WorldManager.h
@@ -0,0 +1,20 @@
+/*
+ * WorldManager.h
+ *
+ *  Created on: Nov 22, 2008
+ *      Author: twk
+ */
+
+#ifndef WORLDMANAGER_H_
+#define WORLDMANAGER_H_
+
+#include "../common/Thread.h"
+
+class WorldManager: public Thread
+{
+public:
+	WorldManager();
+	virtual ~WorldManager();
+};
+
+#endif /* WORLDMANAGER_H_ */
