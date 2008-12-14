8a81aaa26105817bce535911daaa67d30baba2e7
twk.theainur <twk.theainur@37e2baaa-b253-11dd-9381-bf584fb1fa83>
2008-12-14 14:01:49 +0000

    PY's changes are now integrated into the main branch

--- trunk/src/vortex-client/Application.h
+++ trunk/src/vortex-client/Application.h
@@ -1,151 +1,81 @@
-#include "Application.h"
+#ifndef APPLICATION_H_INCLUDED
+#define APPLICATION_H_INCLUDED
 
-Application::~Application()
+#include <Ogre.h>
+#include <OIS/OIS.h>
+#include "VortexFrameListener.h"
+//#include <CEGUI/CEGUI.h>
+//#include <OgreCEGUIRenderer.h>
+
+using namespace Ogre;
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+#include <CoreFoundation/CoreFoundation.h>
+
+// This function will locate the path to our application on OS X,
+// unlike windows you can not rely on the curent working directory
+// for locating your configuration files and resources.
+std::string macBundlePath()
 {
-  mInputManager->destroyInputObject(mKeyboard);
-  OIS::InputManager::destroyInputSystem(mInputManager);
-  
-  /*//pour CEGUI :
-  delete mRenderer;
-  delete mSystem;*/
+    char path[1024];
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    assert(mainBundle);
 
-      delete mListener;
-      delete mRoot;
+    CFURLRef mainBundleURL = CFBundleCopyBundleURL(mainBundle);
+    assert(mainBundleURL);
+
+    CFStringRef cfStringRef = CFURLCopyFileSystemPath( mainBundleURL, kCFURLPOSIXPathStyle);
+    assert(cfStringRef);
+
+    CFStringGetCString(cfStringRef, path, 1024, kCFStringEncodingASCII);
+
+    CFRelease(mainBundleURL);
+    CFRelease(cfStringRef);
+
+    return std::string(path);
 }
+#endif
 
-void Application::go()
+
+
+class Application
 {
-  createRoot(); // on cr�� la racine
-  defineResources(); // on d�finit les ressources utilis�es par Ogre
-  setupRenderSystem();
-  createRenderWindow(); // on cr�� la fen�tre d'affichage de la sc�ne
-  initializeResourceGroups(); // on initialise les ressources
-  setupScene(); // on installe les �l�ments de la sc�ne
-  setupInputSystem();
-  //setupCEGUI();
-  createFrameListener(); // construction du FrameListener
-  startRenderLoop(); // on commence la boucle de rendu
-}
+public:
 
+    ~Application();
 
+    void go();
 
-void Application::createRoot()
-{
-  #if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
-  mRoot = new Root(macBundlePath() + "/Contents/Resources/plugins.cfg");
-  #else
-  mRoot = new Root();
-  #endif
-}
+private:
+    Root *mRoot;
+    OIS::Keyboard *mKeyboard;
+    OIS::InputManager *mInputManager;
+    //CEGUI::OgreCEGUIRenderer *mRenderer;
+    //CEGUI::System *mSystem;
+    VortexFrameListener *mListener;
+    Camera *mCamera;
+    SceneManager *mSceneMgr;
+    RenderWindow *win;
 
-void Application::defineResources()
-{
-  String secName, typeName, archName;
-  ConfigFile cf;
-  #if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
-  cf.load(macBundlePath() + "/Contents/Resources/resources.cfg");
-  #else
-  cf.load("resources.cfg");
-  #endif
-  ConfigFile::SectionIterator seci = cf.getSectionIterator();
-  while (seci.hasMoreElements())
-  {
-    secName = seci.peekNextKey();
-    ConfigFile::SettingsMultiMap *settings = seci.getNext();
-    ConfigFile::SettingsMultiMap::iterator i;
-    for (i = settings->begin(); i != settings->end(); ++i)
-    {
-      typeName = i->first;
-      archName = i->second;
-      #if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
-      ResourceGroupManager::getSingleton().addResourceLocation( String(macBundlePath() + "/" + archName), typeName, secName);
-      #else
-      ResourceGroupManager::getSingleton().addResourceLocation( archName, typeName, secName);
-      #endif
-    }
-  }
-}
+    void createRoot();
 
-void Application::setupRenderSystem()
-{
-  if (!mRoot->restoreConfig() && !mRoot->showConfigDialog())
-    throw Exception(52, "User canceled the config dialog!", "Application::setupRenderSystem()");
-}
+    void defineResources();
 
-void Application::createRenderWindow()
-{
-  mRoot->initialise(true, "Vortex");
-}
+    void setupRenderSystem();
 
-void Application::initializeResourceGroups()
-{
-  TextureManager::getSingleton().setDefaultNumMipmaps(5);
-  ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
-}
+    void createRenderWindow();
 
-void Application::setupScene()
-{
-  mSceneMgr = mRoot->createSceneManager(ST_INTERIOR, "BspSceneManager");
-  mCamera = mSceneMgr->createCamera("Camera"); // on cr�� la camera
-  mCamera->setNearClipDistance(5);
-  mCamera->pitch(Degree(90)); // On redresse les axes de l'espace pour avoir un d�placement correct de la cam�ra
-  // (les axes sont invers�s entre le moteur quake et ogre)
-  mCamera->setFixedYawAxis(true, Vector3::UNIT_Z); // idem (suite)
-  
-  SceneNode *node = mSceneMgr->getRootSceneNode()->createChildSceneNode("Node");
-  node = mSceneMgr->getRootSceneNode()->createChildSceneNode("CamNode1", Vector3(-850,150,150)); // on place la cam dans l'entr�e
-  
-  node->setFixedYawAxis(true, Vector3::UNIT_Z); // on redresse l'axe de la node �galement
-  
-  node->attachObject(mCamera); // on attache la camera au noeud
-  
-  ////////////////////////////////////////////////////////
-  // ici il faudra aussi y attacher le mod�le du joueur //
-  ////////////////////////////////////////////////////////
-  
-  Viewport *vp = mRoot->getAutoCreatedWindow()->addViewport(mCamera);
-  mSceneMgr->setWorldGeometry("maps/PT.bsp"); // chargement de la map
-  mSceneMgr->setSkyBox(true, "coucher_soleil"); //chargement de la skybox
-}
+    void initializeResourceGroups();
 
-void Application::setupInputSystem()
-{
-  size_t windowHnd = 0;
-  std::ostringstream windowHndStr;
-  OIS::ParamList pl;
-  win = mRoot->getAutoCreatedWindow();
-  
-  win->getCustomAttribute("WINDOW", &windowHnd);
-  windowHndStr << windowHnd;
-  pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
-  mInputManager = OIS::InputManager::createInputSystem(pl);
-  
-  try
-  {
-    mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, false));
-    //mMouse = static_cast<OIS::Mouse*>(mInputManager->createInputObject(OIS::OISMouse, false));
-  }
-  catch (const OIS::Exception &e)
-  {
-    throw Exception(42, e.eText, "Application::setupInputSystem");
-  }
-}
+    void setupScene();
 
-/*void Application::setupCEGUI()
-{
-  }*/
+    void setupInputSystem();
 
-    void Application::createFrameListener()
-    {
-      mListener = new VortexFrameListener(win, mCamera, mSceneMgr);
-      mRoot->addFrameListener(mListener);
-      
-      // On ne montre pas les frames de stats
-      mListener->showDebugOverlay(false);
-    }
-    
-    void Application::startRenderLoop()
-    {
-      mRoot->startRendering();
-    }
-    
+    // void setupCEGUI();
+
+    void createFrameListener();
+
+    void startRenderLoop();
+};
+
+#endif // APPLICATION_H_INCLUDED
