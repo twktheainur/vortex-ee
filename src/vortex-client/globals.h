8757d272d00849dcecb3d037363c450552b0a602
twk.theainur <twk.theainur@37e2baaa-b253-11dd-9381-bf584fb1fa83>
2009-01-17 11:41:29 +0000

    Added CB worspace, added globals.h

---
+++ src/vortex-client/globals.h
@@ -0,0 +1,15 @@
+/*
+ * File:   globals.h
+ * Author: twk
+ *
+ * Created on January 10, 2009, 1:01 PM
+ */
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+#include "../common/Event.h"
+extern Event eventManagerEvent;
+extern Event connectionManagerEvent;
+extern Event worldManagerEvent;
+#endif
+
+
---
+++ src/vortex-server/globals.h
@@ -0,0 +1,16 @@
+/*
+ * File:   globals.h
+ * Author: twk
+ *
+ * Created on January 10, 2009, 1:01 PM
+ */
+#ifndef __GLOBALS_H__
+#define __GLOBALS_H__
+#include "../common/Event.h"
+extern Event eventManagerEvent;
+extern Event chatManagerEvent;
+extern Event connectionManagerEvent;
+extern Event worldManagerEvent;
+#endif
+
+
