044ac48e62c6ef772b8afa1c012829e067977993
alexandre.zahm <alexandre.zahm@37e2baaa-b253-11dd-9381-bf584fb1fa83>
2009-01-28 09:32:19 +0000

    les classes de la bd

--- trunk/src/vortex-server/Document.h
+++ trunk/src/vortex-server/Document.h
@@ -1,41 +1,49 @@
-#ifndef Document
-#define Document
+#ifndef DOCUMENT_H_
+#define DOCUMENT_H_
+
+#include <string>
+#include "Base.h"
+#include <mysql++.h>
+#include <stdio.h>
+using namespace std;
 
 class Document
-{ // idDoc, idAuteur, Commentaire, type
+{
     public:
+    
 		Document();
-		//constructeur par défaut (quand on a besoin de créer un Document vide)
+		//constructeur par dfaut (quand on a besoin de crer un Document vide)
 	
-		Document(String id);
-		//le constructeur d'un Document déjà présent dans la base
+		Document(string id);
+		//le constructeur d'un Document dj prsent dans la base
 		
-		String getIdDoc();
-		//renvoie l'id du doc à partir de l'attribut
+		string getIdDoc();
+		//renvoie l'id du doc  partir de l'attribut
 		
-		String getAuteur();
-		//renvoie l'auteur du doc à partir de la base
+		string getAuteur();
+		//renvoie l'auteur du doc  partir de la base
 		
-		String getCommentaire();
-		//renvoie le commentaire explicatif du document laissé par l'auteur à partir de la base
+		string getType();
+		//renvoie le type du doc  partir de la base
 		
-		int getType();
-		//renvoie le type du doc à partir de la base
-		
-		void setIdDoc(String idDoc);
+		void setIdDoc(string idDoc);
 		//affecte un id au doc (attribut de la classe)
 		
-		void setAuteur(String idAuteur);
+		void setAuteur(string idAuteur);
 		//affecte un idUser au doc dans la base
 		
-		void setCommentaire(String com);
-		//affecte lecommentaire explicatif du document laissé par l'auteur au doc dans la base
+		void setCommentaire(string com);
+		//affecte lecommentaire explicatif du document laiss par l'auteur au doc dans la base
 		
 		void setType(int type);
-		//affecte le type au doc {video,image,son} identifiés par un chiffre dans la base
+		//affecte le type au doc {video,image,son} identifis par un chiffre dans la base
 
     private:
-		String idDoc; //nom physique du document
+		string idDoc; //nom physique du document
+		string auteur;
+		MYSQL *pConnection;
+		enum type { VIDEO, SON, IMAGE };
+		
 };
 
 #endif
